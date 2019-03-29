3bdb89394e42f098d3313acf50f36247b24d6a2e
Collin Meyers <cfmeyers@gmail.com>
2019-03-29 12:34:42 -0400

    Add fixtures, ability to collect model names

--- linker_for_dbt_docs.py
+++ linker_for_dbt_docs.py
@@ -1,3 +1,8 @@
+import glob
+import os
+import re
+
+
 def make_link(project_name, model_name):
     if '.' in model_name:
         file_name = model_name.replace('.', '_', 1)
@@ -6,6 +11,27 @@ def make_link(project_name, model_name):
         file_name = model_name
         display_name = model_name.replace('_', '.', 1)
     return f'<a href="#!/model/model.{project_name}.{file_name}">{display_name}</a>'
+
+
+def collect_linkable_file_names(absolute_path):
+    file_names = [
+        f for f in glob.glob(absolute_path + "/models/**/*.sql", recursive=True)
+    ]
+    return set([os.path.basename(name).split('.')[0] for name in file_names])
+
+
+def make_link_map(model_names, project_name):
+    link_map = {}
+    for model_name in model_names:
+        link_map[model_name] = make_link(project_name, model_name)
+    return link_map
+
+
+def insert_links_in_line(link_map, line):
+    for model_name, link in link_map.items():
+        alternate_name = model_name.replace('_', '.')
+        line = re.sub(model_name + '|' + alternate_name, link, line)
+    return line
 
 
 if __name__ == '__main__':
--- test_linker_for_dbt_docs.py
+++ test_linker_for_dbt_docs.py
@@ -1,15 +1,62 @@
-from linker_for_dbt_docs import make_link
+import os
+
+from linker_for_dbt_docs import (
+    make_link,
+    collect_linkable_file_names,
+    insert_links_in_line,
+    make_link_map,
+)
 
 
 class TestMakeLink:
     def test_it_takes_a_project_name_and_model_with_dot_and_makes_link(self):
-        project_name = 'star_fish'
+        project_name = 'starfish'
         model_name = 'dim.problem_barnacles'
-        expected = '<a href="#!/model/model.star_fish.dim_problem_barnacles">dim.problem_barnacles</a>'
+        expected = '<a href="#!/model/model.starfish.dim_problem_barnacles">dim.problem_barnacles</a>'
         assert expected == make_link(project_name, model_name)
 
     def test_it_takes_a_project_name_and_model_with_underscore_and_makes_link(self):
-        project_name = 'star_fish'
+        project_name = 'starfish'
         model_name = 'dim_problem_barnacles'
-        expected = '<a href="#!/model/model.star_fish.dim_problem_barnacles">dim.problem_barnacles</a>'
+        expected = '<a href="#!/model/model.starfish.dim_problem_barnacles">dim.problem_barnacles</a>'
         assert expected == make_link(project_name, model_name)
+
+
+class TestCollectLinkableFileNames:
+    def test_it_collects_model_names_from_absolute_path(self):
+        absolute_path = os.path.abspath('fixtures/starfish')
+        expected = set(['raw_barnacles', 'dim_problem_barnacles'])
+
+        assert expected == collect_linkable_file_names(absolute_path)
+
+
+class TestInsertLinksInLine:
+    def test_it_inserts_link_for_single_underscored_model_name(self):
+        model_names = set(['raw_barnacles', 'dim_problem_barnacles'])
+        project_name = 'starfish'
+        link_map = make_link_map(model_names, project_name)
+        line = 'Lots of rows in dim_problem_barnacles.'
+
+        expected = 'Lots of rows in <a href="#!/model/model.starfish.dim_problem_barnacles">dim.problem_barnacles</a>.'
+
+        assert expected == insert_links_in_line(link_map, line)
+
+    def test_it_inserts_link_for_single_dotted_model_name(self):
+        model_names = set(['raw_barnacles', 'dim_problem_barnacles'])
+        project_name = 'starfish'
+        link_map = make_link_map(model_names, project_name)
+        line = 'Lots of rows in dim.problem_barnacles.'
+
+        expected = 'Lots of rows in <a href="#!/model/model.starfish.dim_problem_barnacles">dim.problem_barnacles</a>.'
+
+        assert expected == insert_links_in_line(link_map, line)
+
+    def test_it_inserts_link_for_two_model_names(self):
+        model_names = set(['raw_barnacles', 'dim_problem_barnacles'])
+        project_name = 'starfish'
+        link_map = make_link_map(model_names, project_name)
+        line = 'Lots of rows in dim.problem_barnacles, dim_problem_barnacles.'
+
+        expected = 'Lots of rows in <a href="#!/model/model.starfish.dim_problem_barnacles">dim.problem_barnacles</a>, <a href="#!/model/model.starfish.dim_problem_barnacles">dim.problem_barnacles</a>.'
+
+        assert expected == insert_links_in_line(link_map, line)
